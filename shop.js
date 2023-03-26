77e5eef2fb72129a12fb8594f0aeb20cc8f252e4
iOllaph <pabllo164164@gmail.com>
2023-03-25 22:05:27 -0300

    Iniciating Javascript

---
+++ shop.js
@@ -0,0 +1,23 @@
+const itemContainer = document.querySelector("item-container");
+
+const itemImage = document.querySelector("item-image");
+const itemTitle = document.querySelector("item-name");
+const itemPrice = document.querySelector("item-price");
+
+
+const item = {
+    Image: itemImage.value ,
+    Title: itemTitle.value , 
+    Price: itemPrice.value
+}
+
+let items = [];
+
+udpateView()
+
+
+function udpateView() {
+    
+}
+
+
